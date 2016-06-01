c60ba149d63600423818a4c83faba34654ce4f29
hkingCNN <hope.king@cnn.com>
2016-06-01 09:55:13 -0400

    updated logic for hello and unknown text
    return bot.say('Welcome to my Facebook page! Got a tip or a comment? Just say HELLO to get started!')
    if (!_.has(scriptRules, upperText)) {
                        return bot.say(`Can you say that another way?`).then(() => 'speak');

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('Welcome to my Facebook page! Got a story tip or a comment? Just say HELLO to get started!')
                 .then(() => 'speak');
         }
     },
@@ -44,7 +44,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`I didn't understand that.`).then(() => 'speak');
+                    return bot.say(`Can you say that another way?`).then(() => 'speak');
                 }
 
                 var response = scriptRules[upperText];
